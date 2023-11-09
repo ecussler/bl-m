59034307f07448a4c2d511679b73a29eb18aa68b
E Cussler <cusslere@gmail.com>
2023-11-09 14:04:25 -0800

    Plant model setup

---
+++ server/models/plant.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose'); 
+
+const Schema = mongoose.Schema; 
+
+const plantsSchema = new Schema({
+    commonName: {
+        type: String, 
+        required: true
+    }, 
+    scientificName: {
+        type: String, 
+        required: true
+    }, 
+    careLevel: {
+        type: String, 
+        required: true
+    }, 
+    cycle: {
+        type: String, 
+        required: false
+    }, 
+    watering: {
+        type: String, 
+    }, 
+    sunlight: {
+        type: String, 
+    }, 
+    flowering: {
+        type: Boolean, 
+    }, 
+    petToxin: {
+        type: Boolean, 
+    },
+    description: {
+        type: String,
+        required: true, 
+    }, 
+    thumbnail: {
+        type: String,
+    }, 
+
+})
